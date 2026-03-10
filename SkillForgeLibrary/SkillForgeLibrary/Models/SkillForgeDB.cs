af01123a94b511bcb0f844f2b2b130c222608473
jashwanthreddypulugu <Pulugu.JashwanthReddy@cognizant.com>
2026-03-10 16:43:28 +0530

    Add AuditLog DbSet to SkillForgeDB

--- SkillForgeLibrary/SkillForgeLibrary/Models/SkillForgeDB.cs
+++ SkillForgeLibrary/SkillForgeLibrary/Models/SkillForgeDB.cs
@@ -16,6 +16,7 @@ public class SkillForgeDB:DbContext
     public virtual DbSet<Course> Courses{ get; set; }
     public virtual DbSet<Module> Modules { get; set; }
     public virtual DbSet<User> Trainers { get; set; }
+    public virtual DbSet<AuditLog> AuditLog {get; set; }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer("Data Source=LTIN718736\\SQLEXPRESS;Initial Catalog=SkillForgeDB;Integrated Security=True;TrustServerCertificate=True");
